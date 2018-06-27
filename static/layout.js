924b07994f5cbe734d4be3f5b46bc301de2070c7
Kiran Prabhat <prabhat.kiran@sap.com>
2018-06-27 04:54:08 +0000

    UI enhancements and speech integration

--- static/layout.js
+++ static/layout.js
@@ -13,148 +13,344 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 (function() {
-  "use strict";
+	"use strict";
 
-  var ENTER_KEY_CODE = 13;
-  var queryInput, resultDiv, accessTokenInput,contractfile;
-  var defaultText = 'I did not get that , You can ask me<ol><li>Conditions of Contract</li><li>Review of contract</li><li>Title of contract</li><li>whether contract is active</li></ol>';
-  window.onload = init;
+	var ENTER_KEY_CODE = 13;
+	var queryInput, resultDiv, accessTokenInput, contractfile;
+	var defaultText =
+		'I did not get that , You can ask me<ol><li>Conditions of Contract</li><li>Review of contract</li><li>Title of contract</li><li>whether contract is active</li></ol>';
 
-  function init() {
-    queryInput = document.getElementById("q");
-    resultDiv = document.getElementById("result");
-    contractfile = document.getElementById("contractfile");
-    var uploadContractButton = document.getElementById("upload_contract");
-    document.getElementById("main-wrapper").style.display = "none";
-    queryInput.addEventListener("keydown", queryInputKeyDown);
-    uploadContractButton.addEventListener("click", uploadContract);
-   
-  }
-  
-  
-  function uploadContract(){
-	  var file;
-	  contractfile.files.length > 0 ? file = contractfile.files[0] : file="No file is uploaded"; 	 
-    var fd = new FormData();
-    fd.append('file', $('input[type=file]')[0].files[0])
-    $.ajax({
-      url: './uploader',  
-      type: 'POST',
-      data: fd,
-      success:function(data){
-          alert(data);
-      },
-      cache: false,
-      contentType: false,
-      processData: false
-  });
-	  document.getElementById("main-wrapper").style.display = "block";
-	  setAccessToken();
-  }
-  
+	window.onload = init;
 
-  function setAccessToken() {
-    window.init();
-  }
+	function init() {
+		queryInput = document.getElementById("q");
+		resultDiv = document.getElementById("result");
+		contractfile = document.getElementById("contractfile");
+		var uploadContractButton = document.getElementById("upload_contract");
+		document.getElementById("main-wrapper").style.display = "none";
+		if (queryInput !== null) {
+			queryInput.addEventListener("keydown", queryInputKeyDown);
+		}
+		if (uploadContractButton !== null) {
+			uploadContractButton.addEventListener("click", uploadContract);
+		}
+	}
 
-  function queryInputKeyDown(event) {
-    if (event.which !== ENTER_KEY_CODE) {
-      return;
-    }
+	function uploadContract() {
+		var file;
+		contractfile.files.length > 0 ? file = contractfile.files[0] : file = "No file is uploaded";
+		var fd = new FormData();
+		fd.append('file', $('input[type=file]')[0].files[0])
+		$.ajax({
+			url: './uploader',
+			type: 'POST',
+			data: fd,
+			success: function(data) {
+				alert(data);
+			},
+			cache: false,
+			contentType: false,
+			processData: false
+		});
+		document.getElementById("main-wrapper").style.display = "block";
+		setAccessToken();
+	}
 
-    var value = queryInput.value;
-    queryInput.value = "";
+	function setAccessToken() {
+		window.init();
+	}
 
-    createQueryNode(value);
-    var responseNode = createResponseNode();
+	function queryInputKeyDown(event) {
+		if (event.which !== ENTER_KEY_CODE) {
+			return;
+		}
 
-    sendText(value)
-      .then(function(response) {
-        var result;
-        try {
-          result = response.result.fulfillment.speech
-        } catch(error) {
-          result = "";
-        }
-        sendToServer(result,responseNode);
-        
-      })
-      .catch(function(err) {
-        setResponseOnNode("Something goes wrong", responseNode);
-      });
-  }
-  
-  function sendToServer(query,responseNode){
-	  var url="/contractadvisor/"
-	  var response;
-	  if(query===defaultText){
-		  setResponseOnNode(query,responseNode);
-		  return;  
-	  }
-	  
-	  else if(query.includes('Title')){
-		  url=url+"title";
-	  }
-	  
-	  else if(query.includes('Review')){
-		  url=url+"suspicious";
-	  }
-	  else if(query.includes("Conditions")){
-		  url=url+"conditions";
-	  }
-	  else if(query.includes("Active")){
-		  url=url+"startdate";
-	  }
-	  $.ajax({
-	      url: url,  
-	      type: 'GET',
-	      success:function(data){
-	          response=data;
-	          
-	          if(query.includes("Conditions")){
-	          data=data.replace("[[", "");
-	          data=data.replace("[", "");
-	          data=data.replace("[", "");
-	          data=data.split(", \"\"]");
-	          data.pop();
-	          response='<ol><li>' + data.join("</li><li>"); + '</li></ol>'
+		var value = queryInput.value;
+		queryInput.value = "";
 
-	          }
-	          setResponseOnNode(response, responseNode);
-	      },
-	      error: function(error){
-		      	alert("Error Occured:"+error)
-		  },
-	      cache: false,
-	      contentType: false,
-	      processData: false
-	  });
-  }
+		createQueryNode(value);
 
-  function createQueryNode(query) {
-    var node = document.createElement('div');
-    node.className = "clearfix left-align left card-panel green accent-1";
-    node.innerHTML = query;
-    resultDiv.appendChild(node);
-  }
+		sendText(value)
+			.then(function(response) {
+				var responseNode = createResponseNode();
+				var result;
+				try {
+					result = response.result.fulfillment.speech
+				} catch (error) {
+					result = "";
+				}
+				setResponseOnNode(result, responseNode);
 
-  function createResponseNode() {
-    var node = document.createElement('div');
-    node.className = "clearfix left-align right card-panel blue-text text-darken-2 hoverable";
-    node.innerHTML = "...";
-    resultDiv.appendChild(node);
-    return node;
-  }
+			})
+			.catch(function(err) {
+				var responseNode = createResponseNode();
+				setResponseOnNode("Something goes wrong", responseNode);
+			});
+	}
 
-  function setResponseOnNode(response, node) {
-    node.innerHTML = response ? response : "[empty response]";
-    node.setAttribute('data-actual-response', response);
-  }
+	function sendToServer(query, responseNode) {
+		var url = "/contractadvisor/"
+		var response;
+		if (query === defaultText) {
+			setResponseOnNode(query, responseNode);
+			return;
+		} else if (query.includes('Title')) {
+			url = url + "title";
+		} else if (query.includes('Review')) {
+			url = url + "suspicious";
+		} else if (query.includes("Conditions")) {
+			url = url + "conditions";
+		} else if (query.includes("Active")) {
+			url = url + "startdate";
+		}
+		$.ajax({
+			url: url,
+			type: 'GET',
+			success: function(data) {
+				response = data;
 
-  function sendRequest() {
+				if (query.includes("Conditions")) {
+					data = data.replace("[[", "");
+					data = data.replace("[", "");
+					data = data.replace("[", "");
+					data = data.split(", \"\"]");
+					data.pop();
+					response = '<ol><li>' + data.join("</li><li>"); + '</li></ol>'
 
-  }
+				}
+				setResponseOnNode(response, responseNode);
+			},
+			error: function(error) {
+				alert("Error Occured:" + error)
+			},
+			cache: false,
+			contentType: false,
+			processData: false
+		});
+	}
+
+	function createQueryNode(query) {
+		var node = document.createElement('div');
+		node.className = "clearfix left-align left card-panel green accent-1";
+		node.innerHTML = query;
+		resultDiv.appendChild(node);
+	}
+
+	function createResponseNode() {
+		var node = document.createElement('div');
+		node.className = "clearfix left-align right card-panel blue-text text-darken-2 hoverable";
+		node.innerHTML = "...";
+		resultDiv.appendChild(node);
+		return node;
+	}
+
+	function setResponseOnNode(response, node) {
+		node.innerHTML = response ? response : "[empty response]";
+		node.setAttribute('data-actual-response', response);
+	}
+
+	function sendRequest() {
+
+	}
+
+	//////////////////---SPPPPEECCHH--------\\\\\\\\\\\\
+
+	$("#rec").click(function(event) {
+
+		switchRecognition();
+
+	});
+
+	var msg = new SpeechSynthesisUtterance('Hello World');
+
+	$(document).ready(function() {
+
+		$("#q").keypress(function(event) {
+
+			if (event.which == 13) {
+
+				event.preventDefault();
+
+				send();
+
+			}
+
+		});
+
+		$("#rec").click(function(event) {
+
+			switchRecognition();
+
+		});
+
+	});
+
+	var recognition;
+
+	function startRecognition() {
+
+		recognition = new webkitSpeechRecognition();
+
+		recognition.onstart = function(event) {
+
+			updateRec();
+
+		};
+
+		recognition.onresult = function(event) {
+
+			var text = "";
+
+			console.log("recognition");
+
+			for (var i = event.resultIndex; i < event.results.length; ++i) {
+
+				text += event.results[i][0].transcript;
+
+			}
+
+			console.log(text);
+
+			setInput(text);
+
+			stopRecognition();
+
+		};
+
+		recognition.onend = function() {
+
+			stopRecognition();
+
+		};
+
+		recognition.lang = "en-US";
+
+		recognition.start();
+
+	}
+
+	function stopRecognition() {
+
+		if (recognition) {
+
+			recognition.stop();
+
+			recognition = null;
+
+		}
+
+		updateRec();
+
+	}
+
+	function switchRecognition() {
+
+		if (recognition) {
+
+			stopRecognition();
+
+		} else {
+
+			startRecognition();
+
+		}
+
+	}
+
+	function setInput(text) {
+
+		console.log(text);
+
+		$("#q").val(text);
+
+		sendText(text)
+
+		.then(function(response) {
+
+			var result;
+
+			try {
+
+				result = response.result.fulfillment.speech
+
+			} catch (error) {
+
+				result = "";
+
+			}
+
+			var responseNode = createResponseNode();
+
+			setResponseOnNode(result, responseNode);
+
+		})
+
+		.catch(function(err) {
+
+			var responseNode = createResponseNode();
+
+			setResponseOnNode("Something goes wrong", responseNode);
+
+		});
+
+	}
+
+	function updateRec() {
+
+		$("#rec").text(recognition ? "Stop" : "Speak");
+
+	}
+
+	function send() {
+
+		var text = $("#input").val();
+
+		$.ajax({
+
+			type: "POST",
+
+			url: baseUrl + "query?v=20150910",
+
+			contentType: "application/json; charset=utf-8",
+
+			dataType: "json",
+
+			headers: {
+
+				"Authorization": "Bearer " + accessToken
+
+			},
+
+			data: JSON.stringify({
+				query: text,
+				lang: "en",
+				sessionId: "somerandomthing"
+			}),
+
+			success: function(data) {
+
+				//setResponse(JSON.stringify(data, undefined, 2));
+
+				setResponse(data.result.fulfillment.speech);
+
+				var msg = new SpeechSynthesisUtterance(data.result.fulfillment.speech);
+
+				window.speechSynthesis.speak(msg);
+
+			},
+
+			error: function() {
+
+				setResponse("Internal Server Error");
+
+			}
+
+		});
+
+		setResponse("Loading...");
+
+	}
 
 })();
